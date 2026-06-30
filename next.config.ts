4d453001967a76171434bfb5641dbb66a4e197f9
MAKORI JOB <146344763+makorijob@users.noreply.github.com>
2026-06-30 18:18:03 +0300

    Update next.config.ts with new settings
    Enable React strict mode and add experimental features.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  experimental: {
+    nodeMiddleware: true,
+  },
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
