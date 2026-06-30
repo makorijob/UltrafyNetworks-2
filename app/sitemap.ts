2308abcb7341284a637399105b49d47174cd4fde
MAKORI JOB <146344763+makorijob@users.noreply.github.com>
2026-06-30 18:14:13 +0300

    Update base URL in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://const baseUrl = ultrafynetworks-2.pages.dev";
+  const baseUrl = "https://const baseUrl = ultrafynetworks-2.worker.dev";
 
   return [
     {
