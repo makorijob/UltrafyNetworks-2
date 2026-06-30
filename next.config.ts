7ee622b8785fac99a5bbfc8ebc80a9d506c7e01c
Makori Job <makorijob760@gmail.com>
2026-06-30 18:33:15 +0300

    Configure Cloudflare Worker deployment

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  experimental: {
-    nodeMiddleware: true,
-  },
-
   images: {
     unoptimized: true,
   },
